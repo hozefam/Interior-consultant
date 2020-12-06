ab5b5ede7f3aa8bf6a4d1e27b5f4301031853475
Hozefa <hozefam@gmail.com>
2020-12-06 17:20:46 +0300

    Inital Checkin

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: ['Lora'],
+        montserrat: ['Montserrat'],
+        crimsonpro: ['Crimson Pro'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
